891789bfb205590f0c99ec4ae2a6029a02842a96
Alvaro Cabrera <pateketrueke@gmail.com>
2013-03-24 00:49:36 -0600

    Class for stream-context rewrite

---
+++ lib/Haphpy/Module.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Haphpy;
+
+class Module
+{
+
+  protected $stat;
+
+  protected $offset = 0;
+  protected $buffer = '';
+
+
+  public function stream_open($path, $mode, $options, &$opened_path)
+  {
+    $path = str_replace('haphpy://', '', $path);
+    $this->buffer = file_get_contents($path);
+
+    if ($this->buffer === FALSE) {
+      $this->stat = stat($path);
+      return FALSE;
+    }
+
+    $this->buffer = preg_replace('/^\s*<\?php\s*$/', '<' . '?php;', $this->buffer);
+    $this->buffer = preg_replace('/\<\?\=/', '<' . '?php echo ',  $this->buffer);
+
+    $this->buffer = \Haphpy\Parser::parse($this->buffer);
+
+    $this->stat = stat($path);
+
+    return TRUE;
+  }
+
+  public function url_stat()
+  {
+    return $this->stat;
+  }
+
+  public function stream_read($count)
+  {
+    $ret = substr($this->buffer, $this->offset, $count);
+    $this->offset += strlen($ret);
+    return $ret;
+  }
+
+  public function stream_tell()
+  {
+    return $this->offset;
+  }
+
+  public function stream_eof()
+  {
+    return $this->offset >= strlen($this->buffer);
+  }
+
+  public function stream_stat()
+  {
+    return $this->stat;
+  }
+
+  public function stream_seek($offset, $whence)
+  {
+    switch ($whence) {
+      case SEEK_SET;
+        if ($offset < strlen($this->buffer) && $offset >= 0) {
+          $this->offset = $offset;
+          return true;
+        } else {
+          return false;
+        }
+
+      case SEEK_CUR;
+        if ($offset >= 0) {
+          $this->offset += $offset;
+          return true;
+        } else {
+          return false;
+        }
+
+      case SEEK_END;
+        if (strlen($this->buffer) + $offset >= 0) {
+          $this->offset = strlen($this->buffer) + $offset;
+          return true;
+        } else {
+          return false;
+        }
+
+      default;
+        return false;
+    }
+  }
+
+}
